db88b86f4214d1fd4cefcfcbd6184200ecbec8a2
WooMai <woomai@wmlabs.net>
2021-08-25 18:25:49 +0800

    fix storage link

--- app/Providers/ThemeServiceProvider.php
+++ app/Providers/ThemeServiceProvider.php
@@ -26,27 +26,27 @@ class ThemeServiceProvider extends ServiceProvider
     public function boot()
     {
         $theme = config('hstack.theme');
-        //加载路径
-        $views = resource_path("themes/{$theme}");
+        // 加载路径
+        $views = resource_path("themes/$theme");
         $this->loadViewsFrom($views, 'theme');
 
-        //加载翻译
-        $lang= resource_path("themes/{$theme}/lang");
-        $this->loadTranslationsFrom($lang,'theme');//除自带翻译外模板的翻译
+        // 加载翻译
+        $lang = resource_path("themes/$theme/lang");
+        $this->loadTranslationsFrom($lang, 'theme');  // 除自带翻译外模板的翻译
 
-        //静态资源软连接
-        $publicAssets = public_path('assets/theme/'.config('hstack.theme'));
-        if (!is_link(public_path($publicAssets)) && !file_exists($publicAssets)){
+        // 静态资源软连接
+        $publicAssets = public_path('assets/theme/' . config('hstack.theme'));
+        if (!is_link(public_path($publicAssets)) && !file_exists($publicAssets)) {
             $fileSystem = $this->app->files;
             $fileSystem->link(
-                resource_path("themes/".config('hstack.theme').'/assets')
-                ,public_path('assets/theme/'.config('hstack.theme'))
+                resource_path('themes/' . config('hstack.theme') . '/assets'),
+                public_path('assets/theme/' . config('hstack.theme'))
             );
-            //警告用戶不能存儲其他文件，防止安全問題
+            // TODO: 警告用戶不能存儲其他文件，防止安全問題
         }
 
-        //模板全局变量
-        View::share('themeAssets','assets/theme/'.config('hstack.theme'));
+        // 模板全局变量
+        View::share('themeAssets', 'assets/theme/' . config('hstack.theme'));
 
     }
 }
